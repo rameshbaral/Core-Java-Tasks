6ea57682228d54a1ae004003e9c51975efcf22b4
Ramesh <baralramesh07@gmail.com>
2022-10-18 17:05:12 +0545

    Finish Q.NO. 1 of function_exercise

---
+++ src/com/click2career/function_exercise/MainProgram.java
@@ -0,0 +1,12 @@
+package com.click2career.function_exercise;
+
+public class MainProgram {
+    public void display(){
+        System.out.println("Hello, I am called");
+    }
+
+    public static void main(String[] args) {
+        MainProgram mainProgram = new MainProgram();
+        mainProgram.display();
+    }
+}
