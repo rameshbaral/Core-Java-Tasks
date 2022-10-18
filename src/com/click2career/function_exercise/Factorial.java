f1738f35aa86788946e844b594ee15542ba1a698
Ramesh <baralramesh07@gmail.com>
2022-10-18 17:57:24 +0545

    Finish Q.NO. 6 of function_exercise

---
+++ src/com/click2career/function_exercise/Factorial.java
@@ -0,0 +1,29 @@
+/*
+6. Write a program to print the factorial of a number by defining a method
+getFactorial(int number).
+
+ */
+package com.click2career.function_exercise;
+
+import java.util.Scanner;
+
+public class Factorial {
+    //calculates the factorial of the provided nunber
+    private  int getFactorial(int num) {
+        int fact = 1;
+        for (int i=1; i<=num; i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Factorial factorial = new Factorial();
+        System.out.println("Enter the number to calculate factorial");
+        int input = scanner.nextInt();
+        int result = factorial.getFactorial(input);
+        System.out.println("the factorial of "+ input+ " is "+ result);
+
+    }
+}
