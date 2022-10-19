05f947a934189fb1620b960774b1fd2b2c3d8d90
Ramesh <baralramesh07@gmail.com>
2022-10-19 17:50:01 +0545

    Finish Q.NO. 29 of function_exercise

--- src/com/click2career/function_exercise/LeapYear.java
+++ src/com/click2career/function_exercise/LeapYear.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 
 public class LeapYear {
     public static void main(String[] args) {
+        System.out.println("enter a year");
         Scanner scanner = new Scanner(System.in);
         int input = scanner.nextInt();
         System.out.println(isLeapYear(input));
