db4f71cfdb41eed3cf0d11cff325c4fe213886bd
Ramesh <baralramesh07@gmail.com>
2022-10-21 18:14:35 +0545

    Finish Task 1 of this keyword exercise

---
+++ src/com/click2career/abstraction_exercise/this_keyword_exercise/Car.java
@@ -0,0 +1,24 @@
+/*
+1) this: to refer current class instance variable
+ */
+package com.click2career.abstraction_exercise.this_keyword_exercise;
+
+public class Car {
+    int numberOfGears;
+    int topSpeed;
+    String carType;
+
+    public Car(int numberOfGears, int topSpeed, String carType) {
+        this.numberOfGears = numberOfGears;//refers o current class instance variable
+        this.topSpeed = topSpeed;//refers o current class instance variable
+        this.carType = carType;//refers o current class instance variable
+    }
+    public void display(){
+        System.out.println("number of gears in car: "+ numberOfGears + " Top car speed is: "+ topSpeed + " And the Type is "+ carType);
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car(6, 360, "racing");
+        car.display();
+    }
+}
