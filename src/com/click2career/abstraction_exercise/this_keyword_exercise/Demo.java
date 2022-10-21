4ae3988e945790daa0d8cac466b9d4fcdf655a13
Ramesh <baralramesh07@gmail.com>
2022-10-21 18:22:04 +0545

    Finish Task 2 of this keyword exercise

---
+++ src/com/click2career/abstraction_exercise/this_keyword_exercise/Demo.java
@@ -0,0 +1,22 @@
+/*
+2) this: to invoke current class method(Implicitly)
+ */
+package com.click2career.abstraction_exercise.this_keyword_exercise;
+
+public class Demo {
+    public void greet() {
+        System.out.println("hello there");
+    }
+
+    void celebrate() {
+        // this: to invoke current class method(Implicitly) it is same as greet()
+        this.greet();
+        System.out.println("yey!!!!!");
+
+    }
+
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.celebrate();
+    }
+}
