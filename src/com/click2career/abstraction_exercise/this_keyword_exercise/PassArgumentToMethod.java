aa234dfa8ceda8a00ed6691ff006802ee48881d6
Ramesh <baralramesh07@gmail.com>
2022-10-22 13:23:36 +0545

    Finish Task 4 of this keyword exercise

---
+++ src/com/click2career/abstraction_exercise/this_keyword_exercise/PassArgumentToMethod.java
@@ -0,0 +1,19 @@
+/*
+4) this: to pass as an argument in the method
+ */
+package com.click2career.abstraction_exercise.this_keyword_exercise;
+
+public class PassArgumentToMethod {
+    void method(PassArgumentToMethod obj) {
+        System.out.println("method is invoked");
+    }
+
+    void pass() {
+        method(this);
+    }
+
+    public static void main(String[] args) {
+        PassArgumentToMethod passArgumentToMethod = new PassArgumentToMethod();
+        passArgumentToMethod.pass();
+    }
+}
